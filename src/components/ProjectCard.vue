06f38395f15a35bf42d7e4a85d360780582f90f2
federicoverbicaro <federico.verbicaro@icloud.com>
2024-04-05 16:21:32 +0200

    feat: percorso di immagine

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -5,21 +5,38 @@ export default {
   name: 'ProjectCard',
   data() {
     return {
-      arrayWallet: []
+      arrayWallet: [],
+      currentPage: '',
+      lastPage: '',
     }
   },
   methods: {
-    getWallet() {
-      axios.get('http://127.0.0.1:8000/api/portfolio').then(res => {
-        console.log(res.data.wallets)
+    getWallet(walletApiPage) {
+      axios.get('http://127.0.0.1:8000/api/portfolio',
+        {
+          params: {
+            page: walletApiPage
+          }
+        }
 
-        this.arrayWallet = res.data.wallets
+      ).then(res => {
+        console.log(res.data.wallets.data)
+
+        res.data.wallets.data.forEach(element => {
+        element.new_image = 'http://127.0.0.1:8000/storage/images/' + element.new_image;
+      });
+
+
+        this.arrayWallet = res.data.wallets.data
+        this.currentPage = res.data.wallets.current_page
+        this.lastPage = res.data.wallets.last_page
+        
       })
     }
 
   },
   mounted() {
-    this.getWallet()
+    this.getWallet(1)
   },
 }
 
@@ -28,15 +45,43 @@ export default {
 <template>
   <h1>Benvenuti</h1>
 
-  <ul>
-    <li v-for="(element, index) in arrayWallet" :key="element.id">
-     <a href="#"> {{ element.title }}</a>
-    </li>
-  </ul>
+  <div class="card d-flex mt-3" v-for="(element, index) in arrayWallet" :key="element.id">
+    <img class="card-img-top" :src="'http://127.0.0.1:8000/storage/images/' + element.new_image" alt="Title" />
+    <div class="card-body">
+      <h4 class="card-title">{{ element.title }}</h4>
+      <p class="card-text">Description: {{ element.description }}</p>
+      <p class="card-text" v-if="element.category">Category:
+        {{ element.category.name }}
+      </p>
+      <p class="card-text">
+        <span>Lenguace: </span>
+        <span v-for="(tag, tagIndex) in element.tags" :key="tagIndex">
+          {{ tag.name }}
+          <span v-if="tagIndex !== element.tags.length - 1">, </span>
+        </span>
+      </p>
+    </div>
+  </div>
+
+  <div class="mt-5 d-flex justify-content-start">
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item" :class="{ 'disabled': currentPage === 1 }">
+          <button class="page-link" @click="getWallet(currentPage - 1)">Indietro</button>
+        </li>
+        <li class="page-item" v-for="(element, index) in lastPage" :key="index">
+          <button lass="page-link" @click="getWallet(element)">{{ element }}</button>
+        </li>
+        <li class="page-item" :class="{ 'disabled': currentPage === lastPage }">
+          <button class="page-link" @click="getWallet(currentPage + 1)">Avanti</button>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <style scoped>
-ul li{
+ul li {
   text-align: left;
 }
 </style>
